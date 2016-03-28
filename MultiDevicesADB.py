9df2338f048cc09d5443ba3598c3f49988028584
Lucas Liu <anyofyou@gmail.com>
2016-03-28 16:57:41 +0800

    Reformat

--- MultiDevicesADB.py
+++ MultiDevicesADB.py
@@ -1,35 +1,40 @@
 # written by Dary
 
-import os, sys, StringIO , subprocess
+import os, sys, StringIO, subprocess
 
 BLACK, RED, GREEN, YELLOW, BLUE, MAGENTA, CYAN, WHITE = range(8)
 
 adb = '/Applications/Android-SDK/platform-tools/adb'
 
+
 def format(fg=None, bg=None, bright=False, bold=False, dim=False, reset=False):
     # manually derived from http://en.wikipedia.org/wiki/ANSI_escape_code#Codes
     codes = []
-    if reset: codes.append("0")
+    if reset:
+        codes.append("0")
     else:
         if not fg is None: codes.append("3%d" % (fg))
-        #if not fg is None: codes.append("38;5;100")
+        # if not fg is None: codes.append("38;5;100")
         if not bg is None:
-            if not bright: codes.append("4%d" % (bg))
-            else: codes.append("10%d" % (bg))
-        if bold: codes.append("1")
-        elif dim: codes.append("2")
-        else: codes.append("22")
+            if not bright:
+                codes.append("4%d" % (bg))
+            else:
+                codes.append("10%d" % (bg))
+        if bold:
+            codes.append("1")
+        elif dim:
+            codes.append("2")
+        else:
+            codes.append("22")
     return "\033[%sm" % (";".join(codes))
 
 
-#linebuf.write("%s%s%s " % (format(fg=YELLOW,bg=RED, bright=True), "Test", format(reset=True)))
-#line = linebuf.getvalue()
-#print line
-FAILURE_STATUS=['offline','unauthorized']
+FAILURE_STATUS = ['offline', 'unauthorized']
 
-def multiCmd(runCmd):
+
+def multi_cmd(run_cmd):
     cmd = adb + ' ' + ' '.join(sys.argv[1:])
-    # adb get-serialno
+    # adb get serial no
     devices = os.popen(adb + " devices | sed '1,1d' | sed '$d' | cut -f 1").read().splitlines()
     # print devices
     for device in devices[:]:
@@ -45,65 +50,69 @@ def multiCmd(runCmd):
 
     if devices_number == 0:
         print 'No emulators or devices detected - nothing to do.'
-    elif devices_number == 1 and runCmd:
+    elif devices_number == 1 and run_cmd:
         subprocess.call(cmd.split())
     else:
-        devices_model_name=[]
-        devices_versions=[]
+        devices_model_name = []
+        devices_versions = []
         fornum = 0
         for device in devices:
             if FAILURE_STATUS.count(devices_status[fornum]) == 0:
-                model_name_cmd = adb + ' devices | grep ' + device + r' | cut -f 1 | xargs -I $ ' + adb + ' -s $ shell cat /system/build.prop | grep "ro.product.model" | cut -d "=" -f 2 | tr -d '+r'" \r\t\n"'
+                model_name_cmd = adb + ' devices | grep ' + device + r' | cut -f 1 | xargs -I $ ' + adb + ' -s $ shell cat /system/build.prop | grep "ro.product.model" | cut -d "=" -f 2 | tr -d ' + r'" \r\t\n"'
                 platform_versions_cmd = adb + ' devices | grep ' + device + r' | cut -f 1 | xargs -I $ ' + adb + ' -s $ shell cat /system/build.prop | grep "ro.build.version.release" | cut -d "=" -f 2 | tr -d ' + r'"\r\t\n"'
                 devices_model_name.append(os.popen(model_name_cmd).read())
                 devices_versions.append(os.popen(platform_versions_cmd).read())
             else:
                 devices_model_name.append(devices_status[fornum])
                 devices_versions.append(devices_status[fornum])
-            fornum = fornum + 1
-        #print devices_model_name
-        #print devices_versions
-        if runCmd:
+            fornum += 1
+        # print devices_model_name
+        # print devices_versions
+        if run_cmd:
             print 'Multiple devices detected, please select one'
         else:
             print 'List of devices attached'
         num = 1
         for device in devices:
             linebuf = StringIO.StringIO()
-            linebuf.write("%s%s%s " % (format(fg=BLACK,bg=BLACK,bright=True), str(num).strip().center(3), format(reset=True)))
-            linebuf.write("%s%s%s " % (format(fg=YELLOW,bright=True), device.strip()[:20].ljust(20), format(reset=True)))
-            linebuf.write("%s%s%s " % (format(fg=CYAN,bright=True), devices_versions[num-1].strip().center(16), format(reset=True)))
-            linebuf.write("%s%s%s " % (format(fg=MAGENTA,bright=True), devices_model_name[num-1].strip().ljust(20), format(reset=True)))
+            linebuf.write(
+                "%s%s%s " % (format(fg=BLACK, bg=BLACK, bright=True), str(num).strip().center(3), format(reset=True)))
+            linebuf.write(
+                "%s%s%s " % (format(fg=YELLOW, bright=True), device.strip()[:20].ljust(20), format(reset=True)))
+            linebuf.write("%s%s%s " % (
+                format(fg=CYAN, bright=True), devices_versions[num - 1].strip().center(16), format(reset=True)))
+            linebuf.write("%s%s%s " % (
+                format(fg=MAGENTA, bright=True), devices_model_name[num - 1].strip().ljust(20), format(reset=True)))
             line = linebuf.getvalue()
             print line
-            #print str(num)+": " + device + "\t\t" + devices_versions[num-1] + "\t\t"+ devices_model_name[num-1]
-            num = num + 1
-        if runCmd:
+            # print str(num)+": " + device + "\t\t" + devices_versions[num-1] + "\t\t"+ devices_model_name[num-1]
+            num += 1
+        if run_cmd:
             user_input = raw_input("> ")
-            #print user_input
-            if user_input.isdigit() and 0< int(user_input) <= len(devices):
+            # print user_input
+            if user_input.isdigit() and 0 < int(user_input) <= len(devices):
                 print "executing following command:"
-                cmd = adb + " -s " + devices[int(user_input)-1] +' '+ ' '.join(sys.argv[1:])
-                print "    "+cmd+"\n"
+                cmd = adb + " -s " + devices[int(user_input) - 1] + ' ' + ' '.join(sys.argv[1:])
+                print "    " + cmd + "\n"
                 subprocess.call(cmd.split())
             else:
-                print 'You must enter a vaild number'
+                print 'You must enter a valid number'
 
 
-cmd =  adb + ' ' +' '.join(sys.argv[1:])
+cmd = adb + ' ' + ' '.join(sys.argv[1:])
 
-NO_MULTI_COMMAND=['start-server','kill-server','connect','disconnect','help','version','ppp']
+NO_MULTI_COMMAND = ['start-server', 'kill-server', 'connect', 'disconnect', 'help', 'version', 'ppp']
 try:
     first_args = sys.argv[1]
     if first_args == 'devices':
-        multiCmd(False)
+        multi_cmd(False)
     else:
         if NO_MULTI_COMMAND.count(first_args) == 1:
             subprocess.call(cmd.split())
         else:
-            multiCmd(True)
+            multi_cmd(True)
 except KeyboardInterrupt:
     pass
 except:
-    # multiCmd(False)
+    # multi_cmd(False)
     subprocess.call(cmd.split())
